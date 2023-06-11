895ec0ccff0530e801e6a08e472b80448859aeda
Yefree Valdez <yefree13santos@outlook.com>
2023-06-11 19:41:25 +0200

    About section created

---
+++ bb-trickz/src/js/data.js
@@ -0,0 +1,24 @@
+const aboutData = [
+  {
+    title: "Who's She?",
+    image: '/images/desktop/hero/hero-section.png',
+    description: `Bb trickz is a Spanish artist born in Barcelona in 2000 and real name
+          Belize Kazi. His style is characterized by controversial and sometimes
+          even humorous lyrics, and a flow offbeat on rhythms generally of
+          drill, Detroit or even R & B. This has led him to receive numerous
+          comparisons mainly with the American artist Ice Spice.`,
+    link: 'https://genius.com/artists/Bb-trickz',
+  },
+  {
+    title: 'BB Trickz: "Urban Music shops in Humana',
+    image: '/images/desktop/hero/hero-portrait1.png',
+    description: `Tiene las Air Force súper guarras / No sé por qué coño te las compras blancas. 
+          Bb Trickz, nombre artístico de Belize, ha encandilado a las nuevas generaciones al hablar
+          el nuevo lenguaje de la calle. Ya no importa el comunismo sonoro o empezar desde cero, el 
+          proletariado del esfuerzo o la lucha interna, sino preguntarse por qué nadie limpia un modelo 
+          icónico de zapatilla Nike que ha de permanecer prácticamente impoluto.`,
+    link: 'https://www.infobae.com/espana/2023/05/14/bb-trickz-la-musica-urbana-va-al-humana/',
+  },
+];
+
+export default aboutData;
