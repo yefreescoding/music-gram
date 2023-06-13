ea3484d0e9980452c4f868b7a3a23f656bcc7c02
Yefree Valdez <yefree13santos@outlook.com>
2023-06-13 10:20:51 +0200

    Entry section added frontmatter posts

--- bb-trickz/src/content/config.ts
+++ bb-trickz/src/content/config.ts
@@ -5,17 +5,15 @@ import { z, defineCollection } from 'astro:content';
 const artists = defineCollection({
   type: 'content', // v2.5.0 and later
   schema: z.object({
-    title: z.string(),
+    title: z.string().max(60),
     artist: z.string(),
-    description: z.string(),
+    description: z.string().max(120),
     date: z.date(),
     draft: z.boolean(),
-    img: z.object({ src: z.string(), alt: z.string() }),
-    img2: z.object({ src: z.string(), alt: z.string() }),
+    img: z.object({ src: z.string(), alt: z.string() }).optional(),
+    img2: z.object({ src: z.string(), alt: z.string() }).optional(),
   }),
 });
 
 // 3. Export a single `collections` object to register your collection(s)
-export const collections = {
-  blog: artists,
-};
+export const collections = { artists };
